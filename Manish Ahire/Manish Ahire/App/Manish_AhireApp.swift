7a39b8c8da5e3094609cbb71c9b432b2672076de
Manish Ahire <ManishAhire@users.noreply.github.com>
2024-02-28 18:54:02 +0530

    Setup MVVM folder structure and added app icon

---
+++ Manish Ahire/Manish Ahire/App/Manish_AhireApp.swift
@@ -0,0 +1,17 @@
+//
+//  Manish_AhireApp.swift
+//  Manish Ahire
+//
+//  Created by Manish Ahire on 28/02/24.
+//
+
+import SwiftUI
+
+@main
+struct Manish_AhireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
---
+++ Manish Ahire/Manish Ahire/View/Home/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  Manish Ahire
+//
+//  Created by Manish Ahire on 28/02/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
